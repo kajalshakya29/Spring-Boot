7fc950d49aaca484efe9736fc851d36822d1cdc3
kajal shakya <157579940+kajalshakya29@users.noreply.github.com>
2026-01-05 00:54:51 +0530

    Add files via upload

---
+++ MywebApp/src/test/java/com/web/MywebApp/MywebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.MywebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MywebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
