9fced6bf4066d2793d82ffd4e67d43bd3d6b66e6
kajal shakya <157579940+kajalshakya29@users.noreply.github.com>
2026-01-05 00:57:14 +0530

    Add files via upload

---
+++ SpringDataRest/src/main/java/com/RestApp/SpringDataRest/AlienRepo.java
@@ -0,0 +1,9 @@
+package com.RestApp.SpringDataRest;
+
+import com.RestApp.SpringDataRest.model.Alien;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "aliens", path = "aliens")
+public interface AlienRepo extends JpaRepository<Alien, Integer> {
+}
---
+++ SpringDataRest/target/classes/data.sql
@@ -0,0 +1,10 @@
+insert into alien values(101,'kajal', 'java');
+insert into alien values(102,'ananya', 'python');
+insert into alien values(103,'vishakha', 'c');
+insert into alien values(104,'krishna', 'php');
+insert into alien values(105,'kanak', 'rust');
+insert into alien values(106,'dolly', 'java');
+
+
+
+
